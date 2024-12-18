2f6eaa56f45d78386501579fdc4beaf2296a6bbb
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2024-12-18 17:48:19 +0600

    class implemented with blueprint.

--- Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,8 @@ AAuraCharacterBase::AAuraCharacterBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon-> SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon -> SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
