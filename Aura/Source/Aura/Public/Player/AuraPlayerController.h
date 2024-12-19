c9fbdf15ba1262beae487ac5fd77312ce55bc8d5
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2024-12-19 12:59:02 +0600

    Working on player input system
    - Movement input actions and input mapping context created.
    - Aura player controller class is created

---
+++ Aura/Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,29 @@
+// Copyright by FK 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+public:
+	AAuraPlayerController();
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraInputContext;
+	
+};
