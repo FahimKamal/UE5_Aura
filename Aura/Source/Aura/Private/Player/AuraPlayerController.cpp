d79d38aeade57e9bc0c7f5adcf0068409f6737a3
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-01-20 18:19:20 +0600

    Working on move function of player.

---
+++ Aura/Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,44 @@
+// Copyright by FK
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set EnhancedInputSubsystems for player inputs with UInputMappingContext.
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem-> AddMappingContext(AuraContext, 0);
+
+	// Set mouse behaviour in game and UI.
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	SetInputMode(InputModeData);
+	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const struct FInputActionValue& InputActionValue)
+{
+}
