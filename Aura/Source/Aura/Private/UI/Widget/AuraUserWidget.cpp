7d1e93321e795949b171f4c0ab345d5ab8070fce
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-01-27 01:35:11 +0600

    started implementing widget system.

---
+++ Aura/Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright by FK
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
