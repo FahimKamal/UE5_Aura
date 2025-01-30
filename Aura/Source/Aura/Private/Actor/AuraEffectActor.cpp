a02a19895bb3a74d6f6ea45682f810fe4135e83a
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-01-30 12:40:52 +0600

    Now we have functional health and mana potion.

--- Aura/Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Aura/Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -3,6 +3,10 @@
 
 #include "Actor/AuraEffectActor.h"
 
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
 
 AAuraEffectActor::AAuraEffectActor()
 {
@@ -17,9 +21,17 @@ void AAuraEffectActor::BeginPlay()
 	Super::BeginPlay();
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
 
