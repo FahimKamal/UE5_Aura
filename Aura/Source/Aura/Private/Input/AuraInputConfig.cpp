1847fede850f4dfc23054a5ac00571c7bd2fde01
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-02-24 22:35:35 +0600

    Input Actions for different actions are created.

--- Aura/Source/Aura/Private/Input/AuraInputConfig.cpp
+++ Aura/Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -3,3 +3,21 @@
 
 #include "Input/AuraInputConfig.h"
 
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+	const bool bLogNotFound) const
+{
+	for (const auto& [InputAction, InputTag] : AbilityInputActions)
+	{
+		if (InputAction && InputTag == InputTag)
+		{
+			return InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+	}
+
+	return nullptr;
+}
