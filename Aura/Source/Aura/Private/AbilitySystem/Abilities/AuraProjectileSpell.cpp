c6f1976ba2f156fc7acbbce5f9dc876ad76740f4
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-02-26 12:24:42 +0600

    Started working on Projectile c++ class.

---
+++ Aura/Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+// Copyright by FK
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Activate Ability c++"), true, true, FLinearColor::Yellow, 3);
+}
